f6185885698c0bf193aef5c09cc539b2e45c8dbd
Damian Gorlach <damian.gorlach@gmail.com>
2011-09-15 21:49:48 +0200

    typo fix

--- dns-cache.sh
+++ dns-cache.sh
@@ -2,7 +2,7 @@
 
 #sudo cp -f /media/Home/dmn/Projects/resolv.freenet.io.conf /etc/resolv.conf
 cat /etc/resolv.conf | grep -v 127.0.0.1  | cat > /tmp/resolv.conf
-echo 'namesever 127.0.0.1' | sudo tee /etc/resolv.conf
+echo 'nameserver 127.0.0.1' | sudo tee /etc/resolv.conf
 cat /tmp/resolv.conf | sudo tee -a /etc/resolv.conf
 rm -f /tmp/resolv.conf
 #ssh dmn@atom -p 11322 -t sudo dnsmasq -d -C /etc/dnsmasq.freenet.io.conf
