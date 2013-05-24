5b4c07739e8c0d3772dc2a27264cec368a40ae40
Damian Gorlach <damian.gorlach@gmail.com>
2013-05-24 19:55:19 +0200

    comment

--- dmenu.sh
+++ dmenu.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+# also added to ~/.xbindkeysrc
 /usr/bin/dmenu_run -fn '-*-terminus-medium-r-*-*-12-*-*-*-*-*-*-*' -nb rgb:00/00/00 -nf rgb:a0/a0/a0 -sf rgb:d3/d3/d3 -sb rgb:4a/54/63
