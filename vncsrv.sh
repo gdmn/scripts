7dace5925b3d213c743ddce7267f0be8af096489
Damian Gorlach <damian.gorlach@gmail.com>
2013-11-27 21:09:29 +0100

    vncsrv rozdzielone

--- vncsrv.sh
+++ vncsrv.sh
@@ -2,28 +2,14 @@
 
 PASSWORD='dmn'
 
-if [[ "$1" != "" ]]; then
-	DISPLAY="$1"
-else
-	DISPLAY=':0'
-fi
+# -viewonly
+# -scale 0.8 -scale_cursor 1
 
-if [[ "$2" != "" ]]; then
-	RES="$2"
-else
-	RES=''
-fi
-MAX=`xrandr 2>/dev/null | grep maximum | sed 's/.*,//g' | sed 's/.*maximum //g' | sed 's/\W//g'`
+vncsrv_main() {
+	k=`mktemp`
+	echo "$PASSWORD">$k
+	x11vnc -passwdfile rm:$k -many $*
+}
 
-#trap 'xrandr -display :0 -s 1920x1200'
-if [[ "$RES" != "" ]]; then
-	xrandr -display $DISPLAY -s $RES
-fi
+vncsrv_main $*
 
-k=`mktemp`
-echo "$PASSWORD">$k
-x11vnc -display $DISPLAY -passwdfile rm:$k -many
-
-if [[ "$RES" != "" ]]; then
-	xrandr -display $DISPLAY -s $MAX
-fi
