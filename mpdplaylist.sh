6091fec6f069f7d9c843718c25b91dd8cd39f3ad
Damian Gorlach <damian.gorlach@gmail.com>
2015-06-05 09:45:47 +0200

    mpdplaylist.sh

--- mpdplaylist.sh
+++ mpdplaylist.sh
@@ -23,8 +23,7 @@ found() {
 
 parse() {
 	parse_playlist () {
-		grep '/' | sed 's/
-$//g' | while read k; do
+		grep '/' | sed 's/$//g' | while read k; do
 			debug " > read $k"
 			if [[ $k == HTTP* ]] ; then
 				parse "$k"
