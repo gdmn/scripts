d747f40a70a12c624e1722d458cee556eb4a5ed5
Damian Gorlach <damian.gorlach@gmail.com>
2017-03-15 23:15:19 +0100

    ipwan update

--- ipwan.sh
+++ ipwan.sh
@@ -3,3 +3,6 @@
 ip=$(curl -s 192.168.1.1| grep IP|grep -E '.*[0-9]{1,3}(\.[0-9]{1,3}){2}.*' | sed 's/<[^>]*>//g' | sed 's/.*://g' | sed 's/.* //g')
 echo "Router WAN IP: $ip"
 
+digresult=$(dig +noall +answer @8.8.8.8 dyn.h64.pl)
+echo "dyn.h64.pl is resolving to: $digresult"
+
