9e73f5e59c06a9f07ee14e64e86fa1dd932ae146
Damian Gorlach <damian.gorlach@gmail.com>
2020-01-30 21:01:10 +0100

    windows1250toUTF8.sh

---
+++ windows1250toUTF8.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+command -v enconv > /dev/null || \
+	echo 'Install packet "enca"'
+
+command -v enconv > /dev/null || \
+	exit 1
+
+DIR="`pwd`"
+
+if [[ $1 != "" ]]; then
+	DIR="$1"
+fi
+
+pushd "$DIR" > /dev/null
+DIR="`pwd`"
+popd > /dev/null
+
+echo $DIR
+
+find "${DIR}/" -type f \( -iname '*.srt' -o -iname '*.txt' \) -print0 | \
+	xargs -0 -I % bash -c \
+	"cp -fv '%' '%-windows1250.bak' && enconv -x UTF-8 -V '%'"
+
