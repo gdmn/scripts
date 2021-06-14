ad3fd9c480f0f89b8ba62a41e42ff9fe343037ec
Damian <dmn@devsite.pl>
2021-06-14 17:47:09 +0200

    backupwithtimestamp.sh

---
+++ backupwithtimestamp.sh
@@ -0,0 +1,44 @@
+#! /usr/bin/env bash
+
+########################################################################
+# HELP
+
+usage() {
+	echo -e "Usage examples:\n"
+	echo -e "compress: `basename $0` archive_prefix somefile anotherfile directory/"
+}
+
+########################################################################
+# INIT
+
+if [[ "" == "`which zstd`" ]]; then
+	echo 'install zstd (zstandard compression)!'
+	exit 3
+fi
+
+########################################################################
+# COMPRESS
+
+mkarchive() {
+	cd .
+
+	tar --checkpoint=1000 --checkpoint-action=dot -cf - $* | \
+		zstd -T0 --long -o "${OUTPUTFILE}"
+}
+
+OUTPUTFILE="`pwd`/${1}_`date +%Y%m%d_%H%M%S`.tar.zstd"
+
+########################################################################
+# MAIN
+
+
+if [ $# -lt 2 ] ; then
+	usage
+	exit 1
+fi
+
+shift
+
+mkarchive $*
+
+echo -e "To decompress, run: zstd -dc $OUTPUTFILE | tar -x -C outputdirectory/"
