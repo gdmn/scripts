7a0c12e517a67c61a61b1a2cb4dc77eea46885c7
dmn <dmn@devsite.pl>
2022-02-23 09:52:49 +0100

    jdk11mvn.sh updated

--- jdk11mvn.sh
+++ jdk11mvn.sh
@@ -2,8 +2,11 @@
 
 #https://hub.docker.com/_/maven?tab=description
 
-#find . -name target -type d -print0 | sudo xargs -0 -I {} /bin/rm -rf "{}"
+#docker run -v ~/.m2:/var/maven/.m2 -ti --rm -u "$(id -u):$(id -g)" -e MAVEN_CONFIG=/var/maven/.m2 -v "$PWD":/var/maven/project -w /var/maven/project maven:3.6.3-jdk-11 mvn -Duser.home=/var/maven $*
 
-
-docker run -v ~/.m2:/var/maven/.m2 -ti --rm -u "$(id -u):$(id -g)" -e MAVEN_CONFIG=/var/maven/.m2 -v "$PWD":/var/maven/project -w /var/maven/project maven:3.6.3-jdk-11 mvn -Duser.home=/var/maven $*
+mvnver="3"
+jdkver="jdk-11"
+m2dir="$HOME/.m2-${jdkver}"
+mkdir -p "$m2dir"
+docker run -v $m2dir:/var/maven/.m2 -ti --rm -u "$(id -u):$(id -g)" -e MAVEN_CONFIG=/var/maven/.m2 -v "$PWD":/var/maven/project -w /var/maven/project maven:${mvnver}-${jdkver} mvn -Duser.home=/var/maven -Dstyle.color=always $*
 
