7de524651b8f43c61d7d87ee4eb1cb4d52921055
Damian <dmn@devsite.pl>
2024-08-14 12:04:22 +0200

    git-rebase-current-branch.sh

--- git-rebase-current-branch.sh
+++ git-rebase-current-branch.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
 
-git rebase -i $(git merge-base $(git config --get init.defaultBranch || echo master) $(git rev-parse --abbrev-ref HEAD))
+git rebase -i $(git show-branch --merge-base)
+
+# rebase with main branch:
+# git rebase -i $(git merge-base $(git config --get init.defaultBranch || echo master) $(git rev-parse --abbrev-ref HEAD))
 
